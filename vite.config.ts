a25385fc74cfc2081e5e68e09ac73fef3c1f2be4
wangqian <wq@wangqiandeMacBook-Pro.local>
2023-09-23 13:28:24 +0800

    [BugFix]修改

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import Components from 'unplugin-vue-components/vite'
-import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 const pathResolve = (dir:string):string => {
   return resolve(__dirname, '.', dir)
